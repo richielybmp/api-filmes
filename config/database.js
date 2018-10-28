fc9841312ade04cb16a11b410bc5f56abcaccce1
richielybmp <richielybmp@hotmail.com>
2018-10-28 11:14:48 -0300

    Api filmes com NodeJs e MongoDb

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const dbName = 'db_apiFilmes'
+
+//mongoose.connect('mongodb://localhost/'+ dbName, { useNewUrlParser: true })
+mongoose.connect('mongodb://admin:dbadmin1@ds115758.mlab.com:15758/db_apifilmes', { useNewUrlParser: true })
+
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log(`Conectado no banco de dados ${dbName}`);
+});
